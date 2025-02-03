47aad327b03f08dd8217d06f3415bbee603130ce
Lucas Aleixo <lucas.silva@2rpnet.com>
2025-02-03 10:27:50 -0300

    update

--- database/create-db.sql
+++ database/create-db.sql
@@ -4,10 +4,5 @@ CREATE TABLE `cardsgo`.`cardsgo_data` (
   `id_cardsgo` INT NOT NULL AUTO_INCREMENT,
   `expiration` DATETIME NOT NULL,
   `user` VARCHAR(256) NOT NULL,
-  `data` VARCHAR(999) NOT NULL,
+  `data` LONGTEXT NOT NULL,
   PRIMARY KEY (`id_cardsgo`));
-
-INSERT INTO `cardsgo`.`cardsgo_data` (`expiration`, `user`, `data`) VALUES ('2022-06-24', 'aleixo', '{\"todo\": [\"Database card\"],\"doing\": [],\"done\": [],\"blocked\": []}');
-
-
-UPDATE cardsgo.cardsgo_data SET expiration = adddate(DATE_FORMAT(NOW(), '%Y-%m-%d'), INTERVAL 1 MONTH) WHERE user = 'aleixo';
